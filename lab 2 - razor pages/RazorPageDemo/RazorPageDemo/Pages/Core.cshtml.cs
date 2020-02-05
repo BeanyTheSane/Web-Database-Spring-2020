abcb9411604ada40f1192db8ec30d8d1c923c575
Adam Knitter <knitter0288@gmail.com>
2020-02-04 22:26:19 -0500

    Built The whole thing

---
+++ lab 2 - razor pages/RazorPageDemo/RazorPageDemo/Pages/Core.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageDemo.Pages
+{
+    public class CoreModel : PageModel
+    {
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = "What is ASP.NET Core?";
+        }
+    }
+}
