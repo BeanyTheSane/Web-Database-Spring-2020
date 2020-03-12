a480f8db96c41114c8b423c361e27a3eef6a1b6e
Adam Knitter <knitter0288@gmail.com>
2020-03-11 21:01:30 -0400

    wip

---
+++ lab7/RazorPagesMovie/Models/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Actor
+    {
+        public int ID { get; set; }
+        public string ActorId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+    }
+}
